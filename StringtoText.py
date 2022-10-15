21b5b720c18601e2df85418fa034f73174234321
sverre <sverre.94@hotmail.com>
2022-10-15 14:01:44 +0200

    Xor two string

--- StringtoText.py
+++ StringtoText.py
@@ -4,7 +4,7 @@
 
 
 def start():
-    print("velg type som skal gjøres om til text: \n 1 : hex \n 2: binary \n 3: octal")
+    print("velg type som skal gjøres om til text: \n 1 : hex \n 2: binary \n 3: octal \n 4: Xor two strings")
     valgt = int(input())
     if valgt == 1:
         result = hexToText()
@@ -12,6 +12,8 @@ def start():
         result = binaryToText()
     elif valgt == 3:
         result = octalToString()
+    elif valgt == 4:
+        result = XorTwoStrings()
     else:
         print("ugyldig nummer")
 
@@ -67,6 +69,14 @@ def octalToString():
     return result
     #print("Din input: ")
     #print(strConverted)
+
+def XorTwoStrings():
+    firstString = input("\n Enter the first string: ")
+    secondString = input("\n Enter the second string: ")
+    new = [chr(ord(a)^ ord(b)) for a,b in zip(firstString,secondString)]
+    thirdString = "".join(new)
+    result = thirdString
+    return result
 
 
 start()
